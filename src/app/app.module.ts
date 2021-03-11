b8fbc07efc9661222cf37297e0a2fc8daff889ac
Sebastian <jsruiz7411@misena.edu.co>
2021-03-11 16:09:26 -0500

    Componentes creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ListCitasComponent } from './components/list-citas/list-citas.component';
+import { CrearCitasComponent } from './components/crear-citas/crear-citas.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ListCitasComponent,
+    CrearCitasComponent,
     
   ],
   imports: [
