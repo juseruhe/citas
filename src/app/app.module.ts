f0c908bbe0591e2d31def5cc00cc6dd33444d6f5
Sebastian <jsruiz7411@misena.edu.co>
2021-03-12 16:51:15 -0500

    Se llena agregarcitas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ListCitasComponent } from './components/list-citas/list-citas.component';
 import { CrearCitasComponent } from './components/crear-citas/crear-citas.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -14,7 +15,7 @@ import { CrearCitasComponent } from './components/crear-citas/crear-citas.compon
     
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/crear-citas/crear-citas.component.ts
+++ src/app/components/crear-citas/crear-citas.component.ts
@@ -7,9 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearCitasComponent implements OnInit {
 
+  nombre = ""
+  fecha = "";
+  hora =  "";
+  sintomas = "";
+
+  formularioIncorrecto = false;
+
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  agregarCita(){
+    if(this.nombre == "" || 
+    this.fecha == "" ||
+    this.hora == "" ||
+    this.sintomas == ""){
+
+      this.formularioIncorrecto = true;
+
+      return;
+
+    }
+
+    this.formularioIncorrecto == false;
+
+    // Padre
+const cita = {
+
+  nombre: this.nombre,
+  fecha: this.fecha,
+  hora: this.hora,
+  sintomas: this.sintomas
+
+}
+
+console.log(cita)
+
+this.resetCampos();
+
+  }
+
+  resetCampos(){
+    this.nombre = "";
+    this.fecha = "";
+    this.hora = "";
+    this.sintomas = "";
   }
 
 }
